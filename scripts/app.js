568d7640b6ac6eefb2d0d610d521e68c7fe09fe4
Lergin <lergin@gmx.de>
2015-03-25 19:50:13 +0100

    forget to remove a script-tag

--- scripts/app.js
+++ scripts/app.js
@@ -278,15 +278,12 @@ Polymer('permission-edit',{
         }
     ]
 });
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-60277501-2']);
+_gaq.push(['_trackPageview']);
 
-<script>
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-60277501-2']);
-        _gaq.push(['_trackPageview']);
-
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-    </script>
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
